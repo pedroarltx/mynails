24cfe915ead4f770402e79ac334b38d86bc4e84e
Pedro Andrade <pedroandradegt25@gmail.com>
2025-03-05 17:54:16 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,18 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="pt-BR">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Serviços de manicure e cuidados com as unhas por Rafaela Santos." />
+        <meta name="keywords" content="manicure, unhas, cuidados, Rafaela Silva, estética, beleza" />
+        <meta name="author" content="Rafaela Santos" />
+        <meta property="og:title" content="Manicure Rafaela Santos - Cuidados e Estilo para suas Unhas" />
+        <meta property="og:description" content="Agende seu horário para cuidados de unhas com a especialista Rafaela Silva." />
+        <meta property="og:image" content="URL da imagem para visualização em redes sociais" />
+        <meta property="og:url" content="URL da página" />
+        <link rel="icon" href="/caminho/para/favicon.ico" />
+        <title>Manicure Rafaela Silva</title>
+      </head>
       <body>
         <FirebaseProvider>
           <SiteLayout>{children}</SiteLayout>
@@ -19,7 +31,5 @@ export default function RootLayout({
     </html>
   )
 }
-
-
 
 import './globals.css'
